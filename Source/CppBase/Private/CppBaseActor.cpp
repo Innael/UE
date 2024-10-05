85cc41a2d64834d0fcac5ccd1220ce2f9efcc279
Sergey Bolshakov <Illidan.Ice@gmail.com>
2024-10-05 19:52:46 +0300

    InitialCommit

---
+++ Source/CppBase/Private/CppBaseActor.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppBaseActor.h"
+#include "Engine/Engine.h"
+
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();	
+	ShowActorInformation();
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACppBaseActor::ShowInformation() {
+
+	UE_LOG(LogTemp, Display, TEXT("PlayerName %s"), *PlayerName);
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Blue, PlayerName, true, FVector2D(2.0f, 2.0f));
+
+	UE_LOG(LogTemp, Display, TEXT("CppBase is here"));
+	UE_LOG(LogTemp, Warning, TEXT("CppBase класс ошибся"));
+	UE_LOG(LogTemp, Error, TEXT("CppBase eror"));
+
+	UE_LOG(LogTemp, Display, TEXT("Enemy num %d"), EnemyNum);
+	UE_LOG(LogTemp, Display, TEXT("CurrentHealth: %f"), CurrentHealth);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), IsAlive);
+
+}
+
+void ACppBaseActor::ShowActorInformation() {
+	FString ObjName  = GetName();
+	UE_LOG(LogTemp, Display, TEXT("Instance name: %s"), *ObjName);
+	UE_LOG(LogTemp, Display, TEXT("Enemy num %d"), EnemyNum);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), IsAlive);
+}
